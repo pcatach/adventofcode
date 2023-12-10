18287a09b68d90abbbd4b9e54981a3071247d9af
Patrick Catach <p.catach@iwoca.co.uk>
2023-12-10 15:45:04 +0000

    Add day 8

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 use std::fs;
-use std::str::FromStr;
 
 fn main() {
     let file_path = "input.txt";
@@ -8,35 +7,64 @@ fn main() {
 
     let (instructions, network) = parse_map(&map_input);
     println!("{instructions}");
-    // println!("{:?}", network);
+    // println!("{:?}", network );
 
-    let mut current_node: &String = &String::from_str("AAA").unwrap();
+    // let mut current_node: &str = "AAA";
+    let mut current_nodes: Vec<&str> = network
+        .keys()
+        .filter(|&x| (*x).ends_with('A'))
+        .cloned()
+        .collect();
     let mut current_instruction = 0;
     let mut steps = 0;
-    while current_node != "ZZZ" {
-        let left_right = network.get(current_node).unwrap();
-        current_node = if instructions.chars().nth(current_instruction).unwrap() == 'L' {
-            &left_right.0
+    let mut steps_for_each_path: Vec<usize> = Vec::new();
+
+    // while current_node != "ZZZ" {
+    // while !current_nodes.iter().all(|&node| node.ends_with('Z')) {
+    while !current_nodes.is_empty() {
+        // let left_right = network.get(current_node).unwrap();
+
+        // println!("{:?}", current_nodes);
+        let left_right: Vec<(&str, &str)> = current_nodes
+            .iter()
+            .map(|&node| *network.get(node).unwrap())
+            .collect();
+        current_nodes = if instructions.chars().nth(current_instruction).unwrap() == 'L' {
+            // &left_right.0
+            left_right.iter().map(|x| x.0).collect()
         } else {
-            &left_right.1
+            left_right.iter().map(|x| x.1).collect()
         };
         steps += 1;
-        current_instruction = steps % instructions.len();
-        // if steps % 1_000 == 0 {
-        //     println!("Step: {steps}");
-        // }
-    }
 
-    println!("Number of steps: {steps}")
+        let drop_finished: Vec<&str> = current_nodes
+            .iter()
+            .filter(|&x| !(*x).ends_with('Z'))
+            .cloned()
+            .collect();
+
+        for _ in 0..(current_nodes.len() - drop_finished.len()) {
+            steps_for_each_path.push(steps);
+        }
+
+        current_nodes = drop_finished;
+        current_instruction = steps % instructions.len();
+    }
+    let least_common_factor = steps_for_each_path
+        .iter()
+        .copied()
+        .reduce(lcm_of_two_numbers)
+        .unwrap();
+    println!("Number of steps: {least_common_factor}")
 }
 
-fn parse_map(map_input: &str) -> (&str, HashMap<String, (String, String)>) {
+fn parse_map(map_input: &str) -> (&str, HashMap<&str, (&str, &str)>) {
     let instructions = map_input
         .lines()
         .next()
         .expect("First line should contain left/right instructions");
 
-    let mut network: HashMap<String, (String, String)> = HashMap::new();
+    let mut network: HashMap<&str, (&str, &str)> = HashMap::new();
     for node in map_input.strip_prefix(instructions).unwrap().trim().lines() {
         let (element, left_right) = node.split_once(" = ").unwrap();
         let (left, right) = left_right.split_once(',').unwrap();
@@ -44,7 +72,18 @@ fn parse_map(map_input: &str) -> (&str, HashMap<String, (String, String)>) {
         let left = left.strip_prefix('(').unwrap().trim();
         let right = right.strip_suffix(')').unwrap().trim();
 
-        network.insert(element.to_string(), (left.to_string(), right.to_string()));
+        network.insert(element, (left, right));
     }
     (instructions, network)
+}
+
+pub fn lcm_of_two_numbers(a: usize, b: usize) -> usize {
+    a * b / gcd_of_two_numbers(a, b)
+}
+
+fn gcd_of_two_numbers(a: usize, b: usize) -> usize {
+    if b == 0 {
+        return a;
+    }
+    gcd_of_two_numbers(b, a % b)
 }
