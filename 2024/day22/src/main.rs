36479a47cb05af4b7cca744957eaa5f07928f589
Patrick Catach <patrick.catach@gmail.com>
2024-12-24 12:01:39 +0100

    Make day 22 slightly faster

--- 2024/day22/src/main.rs
+++ 2024/day22/src/main.rs
@@ -21,7 +21,7 @@ If it doesn'n see that sequence for a given banana seller, it will jump to the n
 
 What sequence must the monkey look for in order to maximise the number of bananas purchased?
 */
-use std::{collections::{HashMap, VecDeque}, io};
+use std::{collections::{HashMap, HashSet}, io};
 
 use utils::read_from_args;
 
@@ -31,37 +31,34 @@ fn main() -> io::Result<()> {
     let input = read_from_args()?;
 
     let mut sum: u64 = 0;
-    let mut patterns_cache: HashMap<VecDeque<isize>, u64> = HashMap::new(); // pattern of 4 diffs => total number of bananas
+    let mut patterns_cache: HashMap<[isize; 4], u64> = HashMap::new(); // pattern of 4 diffs => total number of bananas
+    let mut seen: HashSet<[isize; 4]> = HashSet::new();
 
     for line in input.lines() {
-        let seed = line.parse::<u64>().unwrap();
-        let mut current = seed;
-        let mut price_diffs = VecDeque::new();
+        let mut secret = line.parse::<u64>().unwrap();
+        let mut previous_price = secret % 10;
 
-        let mut monkey_patterns: HashMap<VecDeque<isize>, u64> = HashMap::new();
-        for _ in 0..MAX_NUMBERS {
-            let next = rng(current);
-            let price_diff = (next % 10) as isize - (current % 10) as isize;
-            current = next;
-            println!("{}, {price_diff}", current % 10);
+        let mut price_diffs = [0; 4];
+
+        for i in 0..MAX_NUMBERS {
+            secret = rng(secret);
+            let price = secret % 10;
+            let price_diff = price as isize - previous_price as isize;
             
-            price_diffs.push_back(price_diff);
-            if price_diffs.len() < 4 {
+            price_diffs = [price_diffs[1], price_diffs[2], price_diffs[3], price_diff];
+            if i <= 3 {
                 continue;
             }
-            monkey_patterns.entry(price_diffs.clone()).or_insert(current % 10);
-            price_diffs.pop_front();
-        }
-        sum += current;
-
-        // merge patterns
-        for (k, v) in monkey_patterns.iter() {
-            if let Some(count) = patterns_cache.get_mut(&k) {
-                *count += v;
-            } else {
-                patterns_cache.insert(k.clone(), *v);
+            
+            if !seen.contains(&price_diffs) {
+                seen.insert(price_diffs);
+                *patterns_cache.entry(price_diffs).or_insert(0) += price;
             }
+
+            previous_price = price;
         }
+        seen.clear();
+        sum += secret;
     }
     let max_pattern = &patterns_cache.iter().max_by_key(|x| *x.1).unwrap();
     dbg!(max_pattern);
@@ -70,8 +67,7 @@ fn main() -> io::Result<()> {
 }
 
 fn rng(s: u64) -> u64 {
-    let mut n = ( (s << 6) ^ s) & (2_u64.pow(24) - 1);
-    n = ( (n >> 5) ^ n) & (2_u64.pow(24) - 1);
-    n = ( (n << 11) ^ n) & (2_u64.pow(24) - 1);
-    return n
+    let mut n = ( (s << 6) ^ s) & 0xFFFFFF;
+    n = (n >> 5) ^ n; // pruning not needed
+    ((n << 11) ^ n) & 0xFFFFFF
 }
